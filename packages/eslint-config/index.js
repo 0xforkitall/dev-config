050d4c47254d422b28d2c8b6bdda26109cbd1019
0xforkitall <0xforkitall@gmail.com>
2023-06-10 17:11:38 +0200

    Remove parser config from eslint configs, bump package version

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -6,7 +6,6 @@ module.exports = {
         'plugin:@typescript-eslint/recommended',
         'plugin:prettier/recommended',
     ],
-    parser: '@typescript-eslint/parser',
     rules: {
         '@typescript-eslint/no-empty-interface': 'off',
         '@typescript-eslint/no-non-null-assertion': 'off',
