c82d63399674a10dec4aa62e2063acff36b11fe6
0xforkitall <0xforkitall@gmail.com>
2023-06-10 15:16:18 +0200

    Initial commit

---
+++ packages/eslint-config/config-base.js
@@ -0,0 +1,8 @@
+'use strict';
+
+module.exports = {
+    extends: [
+        './config-base.js',
+        'next/core-web-vitals',
+    ]
+};
---
+++ packages/eslint-config/config-nextjs.js
@@ -0,0 +1,32 @@
+'use strict';
+
+module.exports = {
+    plugins: ['@typescript-eslint', 'prettier'],
+    extends: [
+        'plugin:@typescript-eslint/recommended',
+        'plugin:prettier/recommended',
+    ],
+    rules: {
+        '@typescript-eslint/no-empty-interface': 'off',
+        '@typescript-eslint/no-non-null-assertion': 'off',
+        '@typescript-eslint/no-explicit-any': 'warn',
+        '@typescript-eslint/no-unused-vars': ['warn', { ignoreRestSiblings: true }],
+        '@typescript-eslint/prefer-nullish-coalescing': 'warn',
+        '@typescript-eslint/array-type': ['warn', { default: 'array-simple' }],
+        '@typescript-eslint/consistent-type-imports': ['warn', { fixStyle: 'inline-type-imports' }],
+        'react/jsx-curly-brace-presence': ['warn', { props: 'never', children: 'never' }],
+        'import/no-cycle': ['warn', { maxDepth: 10 }],
+        'import/no-duplicates': 'warn',
+        'no-console': 'warn',
+        'newline-before-return': 'warn',
+        curly: 'warn',
+        'brace-style': 'warn',
+        'react/self-closing-comp': 'warn',
+        'prefer-template': 'warn',
+        'no-useless-concat': 'warn',
+    },
+    parserOptions: {
+        project: 'tsconfig.json',
+        tsconfigRootDir: __dirname,
+    },
+};
